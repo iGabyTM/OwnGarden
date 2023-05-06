56d3df4de4858dd9b26ed48e6f794b728ea9d6a4
iGabyTM <contactgabytm@gmail.com>
2023-05-06 15:07:44 +0300

    feat: allow fungus to grow on dirt and its variants

--- src/main/java/fr/skyost/owngarden/listener/GlobalEventsListener.kt
+++ src/main/java/fr/skyost/owngarden/listener/GlobalEventsListener.kt
@@ -1,27 +1,32 @@
 package fr.skyost.owngarden.listener
 
 import fr.skyost.owngarden.OwnGarden
-import org.bukkit.Material
+import org.bukkit.*
 import org.bukkit.block.BlockFace
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.world.StructureGrowEvent
+import java.util.concurrent.ThreadLocalRandom
 
 /**
  * Global events handled by the plugin.
  */
 class GlobalEventsListener(private val plugin: OwnGarden) : Listener {
-    /**
-     * Returns the plugin instance.
-     *
-     * @return The plugin instance.
-     */
-    @EventHandler(priority = EventPriority.LOWEST)
+
+    private val fungusSpecies = setOf(TreeType.CRIMSON_FUNGUS, TreeType.WARPED_FUNGUS)
+    private val fungusSaplings = setOf(Material.CRIMSON_FUNGUS, Material.WARPED_FUNGUS)
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     fun onStructureGrow(event: StructureGrowEvent) {
-        if (event.isCancelled) {
+        // Let fungus grow only with bone meal
+        if (fungusSpecies.contains(event.species) && !event.isFromBonemeal) {
+            event.isCancelled = true
             return
         }
+
         val location = event.location
         val schematics = plugin.pluginConfig!!.getSchematics(location.block.type)
         if (plugin.worldEditOperations!!.growTree(schematics, location)) {
@@ -37,6 +42,52 @@ class GlobalEventsListener(private val plugin: OwnGarden) : Listener {
 
             //event.getBlocks().clear();
             event.isCancelled = true
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun onInteract(event: PlayerInteractEvent) {
+        // The player is not right-clicking a block or is not holding an item
+        if (event.action != Action.RIGHT_CLICK_BLOCK || !event.hasItem()) {
+            return
+        }
+
+        val item = event.item ?: return
+
+        // The player is not holding bone meal
+        if (item.type != Material.BONE_MEAL) {
+            return
+        }
+
+        val block = event.clickedBlock ?: return
+
+        // The block is not a fungus
+        if (!fungusSaplings.contains(block.type)) {
+            return
+        }
+
+        // The fungus is not on dirt
+        if (!Tag.DIRT.isTagged(block.getRelative(BlockFace.DOWN).type)) {
+            return
+        }
+
+        val player = event.player
+        @Suppress("DEPRECATION") // the method that takes an int is deprecated, which is also the type of this effect
+        player.playEffect(block.location.clone().add(0.5, 0.5, 0.5), Effect.BONE_MEAL_USE, 10)
+
+        // Remove on bone meal
+        if (player.gameMode != GameMode.CREATIVE) {
+            item.amount = item.amount - 1
+        }
+
+        // 15% chance for the fungus to grow from bone meal
+        if (ThreadLocalRandom.current().nextDouble() < 0.15) {
+            val treeType = if (block.type == Material.CRIMSON_FUNGUS) {
+                TreeType.CRIMSON_FUNGUS
+            } else {
+                TreeType.WARPED_FUNGUS
+            }
+            Bukkit.getPluginManager().callEvent(StructureGrowEvent(block.location, treeType, true, player, emptyList()))
         }
     }
 
